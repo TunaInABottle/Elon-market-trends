f97d907f966cc7a8ed028f871fdd68ea49a24da2
Jana Wicklein <janawicklein@gmail.com>
2022-06-10 17:45:14 +0200

    test push, implemented data generator

--- consumer.py
+++ consumer.py
@@ -1,12 +1,14 @@
 
 
-from kafka import KafkaConsumer, TopicPartition
-consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
+import json 
+from kafka import KafkaConsumer
 
-print("assifning partition")
-consumer.assign([TopicPartition('pizza', 2)])
-
-print("Consuming message")
-msg = next(consumer)
-
-print(msg)
+if __name__ == '__main__':
+    # Kafka Consumer 
+    consumer = KafkaConsumer(
+        'pizza',
+        bootstrap_servers='localhost:9092',
+        auto_offset_reset='earliest'
+    )
+    for message in consumer:
+        print(json.loads(message.value))
