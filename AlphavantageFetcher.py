7087fffc07517b2a25b1b92dbdf56e16ac2e4e30
TunaInABottle <social.tuna+github@pm.me>
2022-06-11 18:32:11 +0200

    Implemented Fetcher dedicated to stock/cryptos

---
+++ AlphavantageFetcher.py
@@ -0,0 +1,49 @@
+import logging
+from MarketType import MarketType
+from Fetcher import Fetcher
+import requests
+import json
+
+
+FORMAT = '%(asctime)s %(clientip)-15s %(user)-8s %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.INFO)
+logger = logging.getLogger('ph')
+
+
+class AlphavantageFetcher(Fetcher):
+    def __init__(self, api_key: str, url: str, type: str, market: str) -> None:
+        super().__init__(api_key, url)
+        self._type = type
+        self._market = market
+
+    def fetch(self) -> json:
+        r = requests.get( 'https://www.alphavantage.co/query?function=' + self._type + '_INTRADAY&symbol=' + self._market + '&market=USD&interval=5min&outputsize=full&apikey=' + self._api_key )
+        return r.json()
+
+
+
+
+
+if __name__ == "__main__":
+
+    with open('stockkey.txt') as f:
+        alphavantage_key = f.read()
+
+
+    fetcherIBM = AlphavantageFetcher(alphavantage_key, "", "TIME_SERIES", "IBM")
+    print( fetcherIBM.fetch() )
+
+    print( "---" )
+
+    fetcherBitcoin = AlphavantageFetcher(alphavantage_key, "", "CRYPTO", "BTC")
+    print( fetcherBitcoin.fetch() )
+
+
+### BITCOIN
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+#url = 'https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=BTC&market=USD&interval=5min&outputsize=full&apikey=' + alphavantage_key
+#r = requests.get(url)
+#data = r.json()
+
+#print(data)
---
+++ Fetcher.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+import json
+
+
+### examples of abstract classhttps://www.pythontutorial.net/python-oop/python-abstract-class/
+### @abstractclass meaning https://www.geeksforgeeks.org/classmethod-in-python/
+
+class Fetcher(ABC):
+    def __init__(self, api_key, url) -> None:
+        self._api_key = api_key
+        self._url = url
+
+    @abstractmethod
+    def fetch(self) -> json:
+        pass
+
+    """"
+
+    @property # says that the next function has to be read as a property 
+    @abstractmethod
+    def _url():
+        raise NotImplementedError
+
+    
+    @property
+    @abstractmethod
+    def _api_key():
+        raise NotImplementedError
+    
+    def print_constant(self):
+        print(type(self)._url)
+    """
