d98afdfa109643a582a20197a224f62c4c0e8205
TunaInABottle <social.tuna+github@pm.me>
2022-06-11 19:19:48 +0200

    declared factory

--- sandbox.py
+++ sandbox.py
@@ -21,7 +21,14 @@ for url_src, markets_info in markets_of_interest.items():
 
 
 
-def is_this_a_factory(source: str): # @TODO I have to answer this question
-    if source == "Alphavantage":
-        pass
-        # @TODO cluster of fetcher, implementation: list where each fetch is identified by type of market and shortname (IMB, BTC, etc)
+def cluster_fetcher_factory(main_source: str, sub_sources: dict) -> FetcherCluster: # @TODO Make a factory of clusters of fetchers, take as parameter the above dictionary and its key
+
+    webSources = {
+        "AlphaVantage": AlphavantageFetcherCluster,
+        "Twitter": TwitterFetcherCluster
+    }
+
+    if main_source not in webSources: # @TODO does this check works?
+        raise Exception(f"cluster_fetcher_factory: {main_source} is not in the list")
+
+    return webSources[main_source](sub_sources)
