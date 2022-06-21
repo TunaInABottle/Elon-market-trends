21ac4a5c1966930c71466896b9216d464855e2fb
TunaInABottle <social.tuna+github@pm.me>
2022-06-21 10:38:13 +0200

    FInished refactoring

--- consumer.py
+++ consumer.py
@@ -2,13 +2,14 @@ import json
 from kafka import KafkaConsumer, TopicPartition
 from setup_logger import fetch_log 
 
-def read_queue(topic: TopicPartition, last_n_messages = 0):
+def read_queue(topic_name: str, partition: int, last_n_messages: int = 0):
     # Kafka Consumer 
     consumer = KafkaConsumer(
         bootstrap_servers='localhost:9092',
         auto_offset_reset='earliest',
         group_id = "my_horse_is_amazing"
     )
+    topic = TopicPartition(topic = topic_name, partition = partition)
     consumer.assign([topic])
     
     last_offset = consumer.position(topic)
@@ -21,34 +22,10 @@ def read_queue(topic: TopicPartition, last_n_messages = 0):
 
         if message.offset == last_offset - 1:
             break
-
+    
+    consumer.close()
     print("Consumer has ended")
 
 
 if __name__ == '__main__':
-    read_queue(TopicPartition(topic = 'TSLA_STOCK', partition = 0), last_n_messages=0)
-
-
-    # # Kafka Consumer 
-    # consumer = KafkaConsumer(
-    #     bootstrap_servers='localhost:9092',
-    #     auto_offset_reset='earliest',
-    #     group_id = "my_horse_is_amazing"
-    # )
-
-    # topic = TopicPartition(topic = 'TSLA_STOCK', partition = 0)
-    # consumer.assign([topic])
-
-    # last_offset = consumer.position(topic)
-    
-    # print(last_offset)
-    # #consumer.seek( topic, last_offset - 100 ) # obtain the last 100 elements
-
-    # for message in consumer:
-    #     print(json.loads(message.value))
-    #     #cfetch_log.info(json.loads(message.value))
-
-    #     if message.offset == last_offset - 1:
-    #         break
-
-    # print("Consumer has ended")
+    read_queue('TSLA_STOCK', 0, last_n_messages = 100 )
