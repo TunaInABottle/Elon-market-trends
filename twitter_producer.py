a2a12ab4c64f351f055852e5d2d7ee7a666a7e77
Jana Wicklein <janawicklein@gmail.com>
2022-07-07 14:46:44 +0200

    updated pipeline with custom Producer

--- twitter_producer.py
+++ twitter_producer.py
@@ -3,6 +3,7 @@ import json
 from datetime import datetime
 import twitter_data
 from kafka import KafkaProducer
+import kafkaCustomProducer
 #from setup_logger import fetch_log 
 
 # Kafka Producer
@@ -17,10 +18,14 @@ given_id = 44196397 #Elon Musk
 if __name__ == '__main__':
     # Infinite loop - runs until you kill the program
     elon_tweets = twitterFetcher.fetch(given_id)
+    #updated to custom Producer class
+    kafkaCustomProducer.write_unique(topic = 'pizza', read_partition = 0, list_elem = elon_tweets.tweets, list_elem_type = twitter_data.Tweet, skip_latest=False )
 
-    for tweet in elon_tweets.tweets:
-        #fetch_log.info(f'Producing message @ {datetime.now()} | Message = {str(tweet.to_repr())}')
-        print(f'Producing message @ {datetime.now()} | Message = {str(tweet.to_repr())}')
-        producer.send('pizza', tweet.to_repr())
-        # Sleep for 3 seconds
-        time.sleep(3)
+    # for tweet in elon_tweets.tweets:
+    #     #fetch_log.info(f'Producing message @ {datetime.now()} | Message = {str(tweet.to_repr())}')
+    #     print(f'Producing message @ {datetime.now()} | Message = {str(tweet.to_repr())}')
+    #     producer.send('pizza', tweet.to_repr())
+    #     # Sleep for 3 seconds
+    #     time.sleep(3)
+
+        
