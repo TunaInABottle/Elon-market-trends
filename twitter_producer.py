1a5ebb8dff8a86779536b44122c79ddb50900fa8
TunaInABottle <social.tuna+github@pm.me>
2022-07-28 19:48:08 +0200

    everything is runnable from init.sh

--- twitter_producer.py
+++ twitter_producer.py
@@ -16,10 +16,16 @@ twitterFetcher = twitter_data.TwitterFetcher()
 given_id = 44196397 #Elon Musk
 
 if __name__ == '__main__':
+    print("twitter_producer executed")
+    break_time = 10 * 60
     # Infinite loop - runs until you kill the program
-    elon_tweets = twitterFetcher.fetch(given_id)
-    #updated to custom Producer class
-    kafkaCustomProducer.write_unique(topic = 'TWEETS', read_partition = 0, list_elem = elon_tweets.tweets, list_elem_type = twitter_data.Tweet, skip_latest=False )
+    while True:
+        elon_tweets = twitterFetcher.fetch(given_id)
+        #updated to custom Producer class
+        kafkaCustomProducer.write_unique(topic = 'TWEETS', read_partition = 0, list_elem = elon_tweets.tweets, list_elem_type = twitter_data.Tweet, skip_latest=False )
+
+        print(f"twitter_fetcher: sleeping for {break_time} seconds")
+        time.sleep( break_time )
 
     # for tweet in elon_tweets.tweets:
     #     #fetch_log.info(f'Producing message @ {datetime.now()} | Message = {str(tweet.to_repr())}')
